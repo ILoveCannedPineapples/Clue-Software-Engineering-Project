1149d3670589ace80e8c5a75a36db3743356b689
JSBTE <js2268@sussex.ac.uk>
2026-05-01 00:12:26 +0100

    Documentation Dice System.cs

--- Assets/Game Scripts/Dice System.cs
+++ Assets/Game Scripts/Dice System.cs
@@ -4,6 +4,13 @@ public class DiceSystem : MonoBehaviour
 
 
 {
+    /*
+     * Function: roll
+     * Input parameters: None
+     * Expected values: No input is required.
+     * Output: Returns an integer between 1 and 6.
+     * Purpose: Simulates rolling a standard six-sided dice.
+     */
     public int roll()
     {
      return Random.Range(1,7); 
