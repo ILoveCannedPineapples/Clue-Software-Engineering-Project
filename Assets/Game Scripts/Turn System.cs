f8b71697a00f3f2fc640ab6dbbe6545764a86999
JSBTE <js2268@sussex.ac.uk>
2026-05-01 00:11:51 +0100

    Documentation Turn System.cs

--- Assets/Game Scripts/Turn System.cs
+++ Assets/Game Scripts/Turn System.cs
@@ -7,11 +7,24 @@ public class TurnSystem : MonoBehaviour
     
         public List<Player> players;
         int currentPlayerIndex = 0;
-
+/*
+     * Function: CurrentPlayer
+     * Input parameters: None
+     * Expected values: The players list should contain at least one Player object.
+     * Output: Returns the Player object whose turn it currently is.
+     * Purpose: Gets the current player based on the currentPlayerIndex value.
+     */
         public Player CurrentPlayer()
     {
         return players[currentPlayerIndex];
     }
+       /*
+     * Function: NextTurn
+     * Input parameters: None
+     * Expected values: The players list should contain at least one Player object.
+     * Output: None
+     * Purpose: Moves the turn to the next player. If the final player has taken their turn, it loops back to the first player.
+     */
         public void NextTurn()
     {
         currentPlayerIndex++;
